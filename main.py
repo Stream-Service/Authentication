1ce378911db4e5ebc1821587c168146691e4e971
OtherUser <otheruser@example.com>
2025-12-25 11:44:53 +0530

    sdgd

--- main.py
+++ main.py
@@ -38,61 +38,10 @@ app.add_middleware(
 def root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-@app.get("/register", response_class=HTMLResponse)
-def register(request: Request):
-    return templates.TemplateResponse("register.html", {"request": request})
-
-@app.get("/upload")
-def show_upload_page(request: Request,curr_user: str = Depends(get_curr_user), db: Session = Depends(get_db)):
-
-    user = get_user_info(curr_user, db)
-    access_token = request.cookies.get("access_token")
-    refresh_token = request.cookies.get("refresh_token")
-    user_data = ResponseUser.from_orm(user).dict()
-
-    return templates.TemplateResponse("upload_file.html", {
-        "request": request,
-        "access": access_token,
-        "refresh": refresh_token,
-        "user_info": user_data,
-        "user_id": curr_user,
-         
-    })
-     
-
-@app.get("/videos")
-def show_upload_page(request: Request,curr_user: str = Depends(get_curr_user), db: Session = Depends(get_db)):
-
-    user = get_user_info(curr_user, db)
-    access_token = request.cookies.get("access_token")
-    refresh_token = request.cookies.get("refresh_token")
-    user_data = ResponseUser.from_orm(user).dict()
-
-    return templates.TemplateResponse("stream_video.html", {
-        "request": request,
-        "access": access_token,
-        "refresh": refresh_token,
-        "user_info": user_data,
-        "crush_id": curr_user,
-         
-    })
-
-@app.get("/pop/{crush_id}")
-def show_post_page(request: Request,crush_id:int,db: Session = Depends(get_db)):
-    user_data = {}  # or skip user info entirely
-    return templates.TemplateResponse("popup.html", {"request": request, "crush_id": crush_id})
-
-
-@app.get("/new")
-def show_post_page(request: Request, db: Session = Depends(get_db)):
-    user_data = {}  # or skip user info entirely
-    return templates.TemplateResponse("newnew.html", {"request": request, "user_info": user_data})
-
-@app.get("/post")
-def show_post_page(request: Request, db: Session = Depends(get_db)):
-    user_data = {}  # or skip user info entirely
-    return templates.TemplateResponse("posts.html", {"request": request, "user_info": user_data})
-
+ 
+    
+ 
+ 
 @app.get('/health')
 def healthcheck():
     
